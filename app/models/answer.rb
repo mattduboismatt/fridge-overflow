f338c4c598da53291d511386e5208c6234cc020f
Armando Bulnes <armandogbulnesjr@gmail.com>
2015-01-16 11:08:52 -0600

    added answers validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,8 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   has_many :responses, as: :responsive
   has_many :votes, as: :votable
+
+  validates :content,
+            presence: true,
+            length: {minimum: 2, maximum: 2000}
 end
